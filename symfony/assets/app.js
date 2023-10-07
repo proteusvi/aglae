7770dd801cdce63238be48b29c1d20ff0d887ac5
Didier <dbeux@infomaniak.ch>
2023-10-07 13:38:08 +0200

    merge

--- symfony/assets/app.js
+++ symfony/assets/app.js
@@ -1,5 +1,3 @@
-import './bootstrap.js';
-// assets/app.js
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -9,6 +7,3 @@ import './bootstrap.js';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-
-// start the Stimulus application
-import './bootstrap';
