7e76b28c6dac0b1b6a65c2ab9d4f92c0efa5fffb
Didier <dbeux@infomaniak.ch>
2023-11-11 17:45:10 +0100

    Installation et configuration de doctrine avec postgres.

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -9,4 +9,7 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
